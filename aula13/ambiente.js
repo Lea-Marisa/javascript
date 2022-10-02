1024cc5fbd46cfbe2d7a62729dc99745717134ff
Léa Santos <lea.marisa.santos5@gmail.com>
2022-10-01 23:04:58 -0300

    Atualizando exercício 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 8)
+
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO (Mais usada nos programas):
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL:
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
